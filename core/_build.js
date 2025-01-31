83391ac3c4c80bc3c9c1632b3deff2609a2adaac
skibidi <jimmuelrivera144@gmail.com>
2025-01-31 13:10:55 +0800

    Update _build.js

--- core/_build.js
+++ core/_build.js
@@ -1,18 +1,16 @@
-import "../cleanup.js";
-
 import {} from "dotenv/config";
 import { writeFileSync } from "fs";
 import { resolve as resolvePath } from "path";
-import logger from "./var/modules/logger.js";
-
+import crypto from "crypto";
+import { execSync } from "child_process";
 import login from "@xaviabot/fca-unofficial";
+import replitDB from "@replit/database";
+
+import logger from "./var/modules/logger.js";
 import startServer from "./dashboard/server/app.js";
 import handleListen from "./handlers/listen.js";
 import environments from "./var/modules/environments.get.js";
 import _init_var from "./var/_init.js";
-
-import replitDB from "@replit/database";
-import { execSync } from "child_process";
 import {
     initDatabase,
     updateJSON,
@@ -21,193 +19,146 @@ import {
     _Users,
 } from "./handlers/database.js";
 
-import crypto from "crypto";
-
 const { isGlitch, isReplit } = environments;
+const TWELVE_HOURS = 1000 * 60 * 60 * 12;
+const TWO_HOURS = 1000 * 60 * 60 * 2;
 
 process.stdout.write(
     String.fromCharCode(27) + "]0;" + "Xavia" + String.fromCharCode(7)
 );
 
-process.on("unhandledRejection", (reason, p) => {
-    console.error(reason, "Unhandled Rejection at Promise", p);
-});
-
-process.on("uncaughtException", (err, origin) => {
-    logger.error("Uncaught Exception: " + err + ": " + origin);
-});
-
-process.on("SIGINT", () => {
-    logger.system(getLang("build.start.exit"));
-    global.shutdown();
-});
-
-process.on("SIGTERM", () => {
-    logger.system(getLang("build.start.exit"));
-    global.shutdown();
-});
-
-process.on("SIGHUP", () => {
-    logger.system(getLang("build.start.exit"));
-    global.shutdown();
-});
-
-async function start() {
-    try {
-        await _init_var();
-
-        logger.system(getLang("build.start.varLoaded"));
-        await initDatabase();
-        global.updateJSON = updateJSON;
-        global.updateMONGO = updateMONGO;
-        global.controllers = {
-            Threads: _Threads,
-            Users: _Users,
-        };
-
-        const serverAdminPassword = getRandomPassword(8);
-        startServer(serverAdminPassword);
-
-        process.env.SERVER_ADMIN_PASSWORD = serverAdminPassword;
-
-        await booting(logger);
-    } catch (err) {
-        logger.error(err);
-        return global.shutdown();
-    }
-}
-
-global.listenerID = null;
-
-function booting(logger) {
-    return new Promise((resolve, reject) => {
-        logger.custom(getLang("build.booting.logging"), "LOGIN");
-
-        loginState()
-            .then(async (api) => {
-                global.api = api;
-                global.botID = api.getCurrentUserID();
-                logger.custom(
-                    getLang("build.booting.logged", { botID }),
-                    "LOGIN"
-                );
-
-                refreshState();
-                global.config.REFRESH ? autoReloadApplication() : null;
-                const newListenerID = generateListenerID();
-                global.listenerID = newListenerID;
-                global.listenMqtt = api.listenMqtt(
-                    await handleListen(newListenerID)
-                );
-                refreshMqtt();
-
-                resolve();
-            })
-            .catch((err) => {
-                if (
-                    isGlitch &&
-                    global.isExists(
-                        resolvePath(process.cwd(), ".data", "appstate.json"),
-                        "file"
-                    )
-                ) {
-                    global.deleteFile(
-                        resolvePath(process.cwd(), ".data", "appstate.json")
-                    );
-                    execSync("refresh");
-                }
-                reject(err);
-            });
+const setupProcessHandlers = () => {
+    process.on("unhandledRejection", (reason, promise) => {
+        console.error("Unhandled Rejection:", reason);
     });
-}
 
-const _12HOUR = 1000 * 60 * 60 * 12;
-const _2HOUR = 1000 * 60 * 60 * 2;
-function refreshState() {
-    global.refreshState = setInterval(() => {
-        logger.custom(getLang("build.refreshState"), "REFRESH");
-        const newAppState = global.api.getAppState();
-        if (global.config.APPSTATE_PROTECTION === true) {
-            if (isGlitch) {
-                writeFileSync(
-                    resolvePath(process.cwd(), ".data", "appstate.json"),
-                    JSON.stringify(newAppState, null, 2),
-                    "utf-8"
+    process.on("uncaughtException", (err, origin) => {
+        logger.error(`Uncaught Exception: ${err} at ${origin}`);
+    });
+
+    ["SIGINT", "SIGTERM", "SIGHUP"].forEach(signal => {
+        process.on(signal, () => {
+            logger.system(getLang("build.start.exit"));
+            global.shutdown();
+        });
+    });
+};
+
+const generateListenerID = () => {
+    return `${Date.now()}-${crypto.randomBytes(4).toString('hex')}`;
+};
+
+const saveAppState = async (api, config) => {
+    const newAppState = api.getAppState();
+    
+    if (config.APPSTATE_PROTECTION && isReplit) {
+        try {
+            const db = new replitDB();
+            const secretKey = await db.get("APPSTATE_SECRET_KEY");
+            if (secretKey) {
+                const encrypted = global.modules.get("aes").encrypt(
+                    JSON.stringify(newAppState),
+                    secretKey
                 );
-            } else if (isReplit) {
-                let APPSTATE_SECRET_KEY;
-                let db = new replitDB();
-                db.get("APPSTATE_SECRET_KEY")
-                    .then((value) => {
-                        if (value !== null) {
-                            APPSTATE_SECRET_KEY = value;
-                            const encryptedAppState = global.modules
-                                .get("aes")
-                                .encrypt(
-                                    JSON.stringify(newAppState),
-                                    APPSTATE_SECRET_KEY
-                                );
-                            writeFileSync(
-                                resolvePath(global.config.APPSTATE_PATH),
-                                JSON.stringify(encryptedAppState),
-                                "utf8"
-                            );
-                        }
-                    })
-                    .catch((err) => {
-                        console.error(err);
-                    });
+                writeFileSync(config.APPSTATE_PATH, JSON.stringify(encrypted));
             }
-        } else {
-            writeFileSync(
-                resolvePath(global.config.APPSTATE_PATH),
-                JSON.stringify(newAppState, null, 2),
-                "utf8"
-            );
+        } catch (err) {
+            logger.error("Failed to save encrypted appstate:", err);
         }
-    }, _12HOUR);
-}
+    } else {
+        const savePath = isGlitch 
+            ? resolvePath(process.cwd(), ".data", "appstate.json")
+            : config.APPSTATE_PATH;
+        writeFileSync(savePath, JSON.stringify(newAppState, null, 2));
+    }
+};
 
-function refreshMqtt() {
+const setupRefreshHandlers = (api) => {
+    global.refreshState = setInterval(async () => {
+        logger.custom(getLang("build.refreshState"), "REFRESH");
+        await saveAppState(api, global.config);
+    }, TWELVE_HOURS);
+
     global.refreshMqtt = setInterval(async () => {
         logger.custom(getLang("build.refreshMqtt"), "REFRESH");
         const newListenerID = generateListenerID();
         global.listenMqtt.stopListening();
         global.listenerID = newListenerID;
-        global.listenMqtt = global.api.listenMqtt(
+        global.listenMqtt = api.listenMqtt(
             await handleListen(newListenerID)
         );
-    }, _2HOUR);
-}
+    }, TWO_HOURS);
+};
 
-function generateListenerID() {
-	return Date.now() + crypto.randomBytes(4).toString('hex');
-}
+const loginWithState = async () => {
+    const { APPSTATE_PATH, APPSTATE_PROTECTION, FCA_OPTIONS } = global.config;
+    
+    try {
+        const appState = await global.modules.get("checkAppstate")(
+            APPSTATE_PATH,
+            APPSTATE_PROTECTION
+        );
 
-function autoReloadApplication() {
-    setTimeout(() => global.restart(), global.config.REFRESH);
-}
-
-function loginState() {
-    const { APPSTATE_PATH, APPSTATE_PROTECTION } = global.config;
-
-    return new Promise((resolve, reject) => {
-        global.modules
-            .get("checkAppstate")(APPSTATE_PATH, APPSTATE_PROTECTION)
-            .then((appState) => {
-                const options = global.config.FCA_OPTIONS;
-
-                login({ appState }, options, (error, api) => {
-                    if (error) {
-                        reject(error.error || error);
-                    }
-                    resolve(api);
-                });
-            })
-            .catch((err) => {
-                reject(err);
+        return new Promise((resolve, reject) => {
+            login({ appState }, FCA_OPTIONS, (error, api) => {
+                if (error) reject(error.error || error);
+                else resolve(api);
             });
-    });
-}
+        });
+    } catch (err) {
+        if (isGlitch) {
+            const appStatePath = resolvePath(process.cwd(), ".data", "appstate.json");
+            if (global.isExists(appStatePath, "file")) {
+                global.deleteFile(appStatePath);
+                execSync("refresh");
+            }
+        }
+        throw err;
+    }
+};
+
+const initializeBot = async (api) => {
+    global.api = api;
+    global.botID = api.getCurrentUserID();
+    logger.custom(getLang("build.booting.logged", { botID: global.botID }), "LOGIN");
+
+    setupRefreshHandlers(api);
+
+    if (global.config.REFRESH) {
+        setTimeout(() => global.restart(), global.config.REFRESH);
+    }
+
+    const newListenerID = generateListenerID();
+    global.listenerID = newListenerID;
+    global.listenMqtt = api.listenMqtt(await handleListen(newListenerID));
+};
+
+const initializeServer = () => {
+    const serverPassword = crypto.randomBytes(4).toString('hex');
+    startServer(serverPassword);
+    process.env.SERVER_ADMIN_PASSWORD = serverPassword;
+};
+
+const start = async () => {
+    try {
+        setupProcessHandlers();
+        await _init_var();
+        logger.system(getLang("build.start.varLoaded"));
+        
+        await initDatabase();
+        global.updateJSON = updateJSON;
+        global.updateMONGO = updateMONGO;
+        global.controllers = { Threads: _Threads, Users: _Users };
+        
+        initializeServer();
+        
+        logger.custom(getLang("build.booting.logging"), "LOGIN");
+        const api = await loginWithState();
+        await initializeBot(api);
+    } catch (err) {
+        logger.error("Startup failed:", err);
+        global.shutdown();
+    }
+};
 
 start();
