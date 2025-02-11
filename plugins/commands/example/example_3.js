612beae13326dd224ee889947bb1b47d27517be4
shuziki <jimmuelrivera144@gmail.com>
2025-02-11 12:01:15 +0800

    Delete plugins/commands/example directory

--- plugins/commands/example/example_3.js
+++
@@ -1,6 +0,0 @@
-// no config provided so will use default config
-// with name will be the file name
-
-export default function ({ message, args, getLang, extra, data, userPermissions, prefix }) {
-    // code here
-}
